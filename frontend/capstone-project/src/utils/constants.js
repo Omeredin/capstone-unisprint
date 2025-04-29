ac1ed4719889f39763e25848a164345b6ddc69ab
Omeredin <106341518+Omeredin@users.noreply.github.com>
2025-04-29 09:48:45 -0400

    new changes

--- frontend/capstone-project/src/utils/constants.js
+++ frontend/capstone-project/src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://capstone.onrender.com' : 'http://localhost:8000'
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://capstone-unisprint.onrender.com' : 'http://localhost:8000'
