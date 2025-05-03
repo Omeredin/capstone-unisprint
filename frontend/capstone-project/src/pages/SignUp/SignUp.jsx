eea80457a42e62ad5fb8edb0c45315f441e98769
Omeredin <106341518+Omeredin@users.noreply.github.com>
2025-05-02 22:48:54 -0400

    new

--- frontend/capstone-project/src/pages/SignUp/SignUp.jsx
+++ frontend/capstone-project/src/pages/SignUp/SignUp.jsx
@@ -59,69 +59,69 @@ const SignUp = () => {
 
   return (
     <>
-    {/* <Navbar_Login_CreateAcc /> */}
+      {/* Create Account */}
 
-    <div class="min-h-screen flex flex-col items-center justify-center w-full dark:bg-gray-950">
-    <img src="../../src/assets/unisprint_logo.webp" className='w-1/6 rounded-full mb-10' alt="" />
-      <div class="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
-        <h1 class="text-2xl font-bold text-center mb-4 text-black">Create an Account</h1>
-        <form onSubmit={handleSignup}>
-        <div class="mb-4">
-          <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Name</label>
-            <input  id="name" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="Name" required
-            
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            />          </div>
-          <div class="mb-4">
-          <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Email Address</label>
-            <input type="email" id="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="your@email.com" required
-            
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />          </div>
+      <div class="min-h-screen flex flex-col items-center justify-center w-full dark:bg-gray-950">
+        <img src="../../src/assets/unisprint_logo.webp" className='w-1/6 rounded-full mb-10' alt="" />
+        <div class="bg-white dark:bg-gray-900 shadow-md rounded-lg px-8 py-6 max-w-md">
+          <h1 class="text-2xl font-bold text-center mb-4 text-black">Create an Account</h1>
+          <form onSubmit={handleSignup}>
             <div class="mb-4">
-          <label for="major" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Major</label>
-            <input  id="major" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="Major" required
-            
-            value={major}
-            onChange={(e) => setMajor(e.target.value)}
-            />          </div>
+              <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Name</label>
+              <input id="name" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="Name" required
+
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />          </div>
             <div class="mb-4">
-          <label for="year" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Year</label>
-          <select class="text-black w-full border border-gray-300 rounded-md p-2 mb-4" value={year}
-            onChange={(e) => setYear(e.target.value)}>
+              <label for="email" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Email Address</label>
+              <input type="email" id="email" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="your@email.com" required
+
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />          </div>
+            <div class="mb-4">
+              <label for="major" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Major</label>
+              <input id="major" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="Major" required
+
+                value={major}
+                onChange={(e) => setMajor(e.target.value)}
+              />          </div>
+            <div class="mb-4">
+              <label for="year" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Year</label>
+              <select class="text-black w-full border border-gray-300 rounded-md p-2 mb-4" value={year}
+                onChange={(e) => setYear(e.target.value)}>
                 <option value="" disabled>Select Year</option>
                 <option>Freshman</option>
                 <option>Sophomore</option>
                 <option>Junior</option>
                 <option>Senior</option>
                 <option>Grad Student</option>
-            </select>
-           
-                     </div>
+              </select>
+
+            </div>
             <div class="mb-4">
-          <label for="hometown" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Hometown <span>(Optional)</span> </label>
-            <input  id="hometown" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="Hometown" required
-            
-            value={hometown}
-            onChange={(e) => setHometown(e.target.value)}
-            />          </div>
+              <label for="hometown" class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-2">Hometown <span>(Optional)</span> </label>
+              <input id="hometown" class="shadow-sm rounded-md w-full px-3 py-2 border border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 text-black" placeholder="Hometown" required
 
-          < PasswordInput
-          value={password}
-          onChange = {(e) => setPassword(e.target.value)}
-          />
-          <div class="flex items-center justify-center mb-4">
-            <div class="flex items-center">
-            
-              <Link to="/login" class="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Already have an account?</Link></div>
-          </div>
+                value={hometown}
+                onChange={(e) => setHometown(e.target.value)}
+              />          </div>
 
-          <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create account</button>
-        </form>
+            < PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div class="flex items-center justify-center mb-4">
+              <div class="flex items-center">
+
+                <Link to="/login" class="text-xs text-indigo-500 hover:text-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Already have an account?</Link></div>
+            </div>
+
+            <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Create account</button>
+          </form>
+        </div>
       </div>
-    </div>
     </>
   )
 }
